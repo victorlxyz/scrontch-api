d422298d16b744694d190eb7a093d6d2146142a8
victorlmtr <victor.lemaitre1@gmail.com>
2025-01-17 03:58:26 +0100

    fixed essentialIngredientDto

--- scrontch.users/src/main/java/xyz/victorl/scrontch/users/dto/EssentialIngredientDto.java
+++ scrontch.users/src/main/java/xyz/victorl/scrontch/users/dto/EssentialIngredientDto.java
@@ -1,5 +1,7 @@
 package xyz.victorl.scrontch.users.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.Value;
 
 import java.io.Serializable;
@@ -7,7 +9,8 @@ import java.io.Serializable;
 /**
  * DTO for {@link xyz.victorl.scrontch.users.entity.EssentialIngredient}
  */
-@Value
+@Getter
+@Setter
 public class EssentialIngredientDto implements Serializable {
     Integer id;
     Integer ingredientid;
