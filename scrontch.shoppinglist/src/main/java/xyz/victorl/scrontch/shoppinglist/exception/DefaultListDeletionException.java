c822253c04cae87c1a4968515f8c1e2d5de6e79d
victorlmtr <victor.lemaitre1@gmail.com>
2025-01-26 08:27:40 +0100

    edit to main pom.xml for annotations

--- scrontch.shoppinglist/src/main/java/xyz/victorl/scrontch/shoppinglist/exception/DefaultListDeletionException.java
+++ scrontch.shoppinglist/src/main/java/xyz/victorl/scrontch/shoppinglist/exception/DefaultListDeletionException.java
@@ -3,5 +3,6 @@ package xyz.victorl.scrontch.shoppinglist.exception;
 public class DefaultListDeletionException extends RuntimeException {
     public DefaultListDeletionException(String message) {
         super(message);
+
     }
 }
