6276f1d3aef5aad14cdd605648a9bb41cdc68ff9
victorlmtr <victor.lemaitre1@gmail.com>
2025-01-26 01:24:25 +0100

    switched to https

--- scrontch.users/src/main/java/xyz/victorl/scrontch/users/service/impl/AuthServiceImpl.java
+++ scrontch.users/src/main/java/xyz/victorl/scrontch/users/service/impl/AuthServiceImpl.java
@@ -65,7 +65,7 @@ public class AuthServiceImpl implements AuthService {
         emailVerificationTokenRepository.save(verificationToken);
 
         // Build verification URL
-        String verificationUrl = "http://victorl.xyz:8086/api/v1/auth/verify-email?token=" + token;
+        String verificationUrl = "https://victorl.xyz:8086/api/v1/auth/verify-email?token=" + token;
 
         // Use EmailNotificationService to send verification email
         emailNotificationService.sendAccountVerificationEmail(user, verificationUrl);
